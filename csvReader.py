937e9a78c62237012524d07151927c11ba81ae8b
Josh Cook <cookiej91@gmail.com>
2023-10-27 15:18:45 +0100

    Update price to be price in pence

--- csvReader.py
+++ csvReader.py
@@ -19,7 +19,7 @@ for index, row in excel_file.iterrows():
     column_b_name = excel_file.columns[1] # vehicleId column
     column_i_name = excel_file.columns[8] # discountPrice column
     value_b = row[column_b_name]
-    value_i = row[column_i_name]
+    value_i = row[column_i_name] * -100 # convert price to pence
     
     # output for each row
     output = f"discountedVehicles.set(\"{value_b}\", {value_i})"
