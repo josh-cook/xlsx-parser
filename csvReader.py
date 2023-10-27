533be9c07e206de876df1c6de055eb2e7f626e85
Josh Cook <cookiej91@gmail.com>
2023-10-27 14:29:53 +0100

    Add column name comments

--- csvReader.py
+++ csvReader.py
@@ -16,8 +16,8 @@ output_list = []
 
 for index, row in excel_file.iterrows():
     # Access the actual column names and values for each row
-    column_b_name = excel_file.columns[1]
-    column_h_name = excel_file.columns[7]
+    column_b_name = excel_file.columns[1] # vehicleId column
+    column_h_name = excel_file.columns[7] # discountPrice column
     value_b = row[column_b_name]
     value_h = row[column_h_name]
     
