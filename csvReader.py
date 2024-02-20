5ff998bc7ff41fd8c17af1aaff76f4b4ce103e20
Josh Cook <joshua.cook@cinch.co.uk>
2024-02-20 16:49:07 +0000

    Update columns for discounted price

--- csvReader.py
+++ csvReader.py
@@ -17,12 +17,12 @@ output_list = []
 for index, row in excel_file.iterrows():
     # Access the actual column names and values for each row
     column_b_name = excel_file.columns[1] # vehicleId column
-    column_h_name = excel_file.columns[7] # discountPrice column
+    column_j_name = excel_file.columns[9] # discountPrice column
     value_b = row[column_b_name]
-    value_h = row[column_h_name] * -100 # convert price to pence
+    value_j = row[column_j_name] * -100 # convert price to pence
     
     # output for each row
-    output = f"discountedVehicles.set(\'{value_b}\', {value_h})"
+    output = f"discountedVehicles.set(\'{value_b}\', {value_j})"
     output_list.append(output)
     
 # Combine the outputs and copy to clipboard
