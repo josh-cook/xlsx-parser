9b53bb6c41219735d2ac695392deb247bfbdd430
Josh Cook <joshua.cook@cinch.co.uk>
2024-10-08 10:55:53 +0100

    Add requirements file for install script

--- install_packages.sh
+++ install_packages.sh
@@ -9,14 +9,8 @@ fi
 source venv/bin/activate
 
 # Install required packages within the virtual environment
-echo "Installing pandas..."
-pip install pandas
-
-echo "Installing pyperclip..."
-pip install pyperclip
-
-echo "Installing openpyxl..."
-pip install openpyxl
+echo "Installing required packages..."
+pip install -r requirements.txt
 
 echo "All packages have been installed in the virtual environment!"
 echo "To activate the virtual environment, run: source venv/bin/activate"
